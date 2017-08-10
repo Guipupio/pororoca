86f2b65f91d27721ede006321d28521f3e14c6cf
guipupio <guicpupio@gmail.com>
2017-08-10 02:14:31 -0300

    fiz uns testes em python.. main.py ainda nao tem nada demais

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
 #Que os jogos comecem :-)
+
+#Iniciamos o programa capturando quantos jogadores participarao..
+
+num_jogadores = input('Insira o numero de jogadores\n')
+
+
+for i in range(0,num_jogadores):
+	print "\rnumero de jogadores =",  num_jogadores
